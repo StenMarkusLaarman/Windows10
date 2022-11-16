1de9dc469c3382c182103b741bbb750f095028e3
StenMarkusLaarman <sten-markus.laarman@voco.ee>
2022-11-16 11:28:36 +0200

    ul123

---
+++ lokaalse_kasutaja_kustutamine_nime_järgi.ps1
@@ -0,0 +1,12 @@
+﻿$Eesnimi = Read-Host -Prompt "Sisesta eesnimi"
+$Perenimi = Read-Host -Prompt "Sisesta perkonna nimi"
+$Kasutajanimi = $Eesnimi + "." + $Perenimi
+$Taisnimi = $Eesnimi + " " + $Perenimi
+try {
+    Remove-LocalUser -Name $Kasutajanimi -ErrorAction stop
+    }
+catch {
+    echo "midagi läks valesti"
+    Exit
+    }
+echo "Kasutaja on kustutatud"
