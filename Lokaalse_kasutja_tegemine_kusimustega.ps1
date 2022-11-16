aad559be92698b0399ab70b35f252319a86791d2
StenMarkusLaarman <sten-markus.laarman@voco.ee>
2022-11-16 11:26:29 +0200

    ul1

---
+++ Lokaalse_kasutja_tegemine_kusimustega.ps1
@@ -0,0 +1,9 @@
+﻿$Eesnimi = Read-Host -Prompt "Sisesta eesnimi"
+$Perenimi = Read-Host -Prompt "Sisesta perkonna nimi"
+$Kasutajanimi = $Eesnimi + "." + $Perenimi
+$Taisnimi = $Eesnimi + " " + $Perenimi
+Write-Output $Taisnimi
+$kontokirjeldus = "skriptiga kasutja"
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+#loome kasutaja kasutadaes parooli
+New-LocalUser $Kasutajanimi -Password $KasutajaParool -FullName $Taisnimi -Description $kontokirjeldus
---
+++ lokaalse_kasutaja_loomine_parameetritega.ps1
@@ -0,0 +1,10 @@
+﻿#kasutaja andmed käsurea parameetrina
+param(
+$kasutajaNimi,
+$taisNimi,
+$kontokirjeldus
+)
+#määrame kasutaja parooli
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+#loome kasutaja kasutades antud andmeid
+New-LocalUser $kasutajaNimi -Password $KasutajaParool -FullName $taisNimi -Description $kontokirjeldus
