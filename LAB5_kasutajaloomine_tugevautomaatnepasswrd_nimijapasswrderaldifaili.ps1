a77eecd077de12913696960a3c4687dddabae57e
StenMarkusLaarman <sten-markus.laarman@voco.ee>
2022-11-28 15:55:34 +0200

    LAB5

---
+++ LAB5_kasutajaloomine_tugevautomaatnepasswrd_nimijapasswrderaldifaili.ps1
@@ -0,0 +1,14 @@
+﻿$Eesnimi = Read-Host -Prompt "Sisesta eesnimi"
+$Perenimi = Read-Host -Prompt "Sisesta perkonna nimi"
+$Kasutajanimi = $Eesnimi + "." + $Perenimi
+$Taisnimi = $Eesnimi + " " + $Perenimi
+Add-Type -AssemblyName System.Web
+$parool = [System.Web.Security.Membership]::GeneratePassword(8,2)
+Write-Output $Taisnimi
+Write-Output $parool
+$kontokirjeldus = "skriptiga kasutja"
+$KasutajaParool = ConvertTo-SecureString $parool -AsPlainText -Force
+#loome kasutaja kasutadaes parooli
+New-LocalUser $Kasutajanimi -Password $KasutajaParool -FullName $Taisnimi -Description $kontokirjeldus
+$Eesnimi | Out-File -FilePath C:\Users\Administrator\Desktop\skriptimine\kasutajanimijapasswrd.txt -Append –NoNewline
+$parool | Out-File -FilePath C:\Users\Administrator\Desktop\skriptimine\kasutajanimijapasswrd.txt -Append
